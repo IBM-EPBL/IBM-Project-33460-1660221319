8db05f31f8ece98f008f7f2ae8e390b354a3cf74
Vijayakumar1907 <vijaykumaar1907@gmail.com>
2022-11-14 14:48:38 +0530

    dbase-connection

---
+++ projectDevelopmentPhase/sprint-2/DB/DbConn.py
@@ -0,0 +1,27 @@
+import ibm_db
+
+hostname="fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud"
+uid="xbg27607"
+pwd="C5YC4sLrSPJknN1k"
+driver="{IBM DB2 ODBC DRIVER}"
+db="bludb"
+port="32731"
+protocol="TCPIP"
+cert="Certificate.crt"
+
+dsn=(
+
+    "DATABASE={0};"
+    "HOSTNAME={1};"
+    "PORT={2};"
+    "UID={3};"
+    "SECURITY=SSL;"
+    "SSLServerCertificate={4};"
+    "PWD={5};"
+).format(db,hostname,port,uid,cert,pwd)
+print(dsn)
+try:
+    db2=ibm_db.connect(dsn,"","")
+    print("Connected to database")
+except:
+    print("Unable to connect",ibm_db.conn_errormsg())
